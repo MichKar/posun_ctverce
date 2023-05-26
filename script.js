6c046e0adaba871dcff0f5f1bb8b76ba05f84638
Michaela <botka@email.cz>
2023-05-26 12:09:34 +0200

    posun čtverce pomocí JavaScriptu - html, css, script

---
+++ script.js
@@ -0,0 +1,24 @@
+let square = document.querySelector(".square")
+
+
+newTop = 100
+newLeft = 200
+
+document.body.addEventListener("keydown", function(e){
+    if (e.key === "ArrowUp"){
+        newTop = newTop -10
+        square.style.top = newTop + "px"
+    } else if (e.key === "ArrowDown"){
+        newTop = newTop +10
+        square.style.top = newTop + "px"
+    } else if (e.key === "ArrowLeft"){
+        newLeft = newLeft -10
+        square.style.left = newLeft + "px"
+    } else if (e.key === "ArrowRight"){
+        newLeft = newLeft +10
+        square.style.left = newLeft + "px"
+    }
+
+
+})
+
