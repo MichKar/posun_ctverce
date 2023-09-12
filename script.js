93b80b50ef421e5c50259dcf02354cdf34803b27
Michaela <botka@email.cz>
2023-09-12 10:26:21 +0200

    posun čtverce pomocí JS

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 let square = document.querySelector(".square")
 
-
 newTop = 100
 newLeft = 200
 
-document.body.addEventListener("keydown", function(e){
-    if (e.key === "ArrowUp"){
-        newTop = newTop -10
+document.body.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowUp") {
+        newTop = newTop - 10
         square.style.top = newTop + "px"
-    } else if (e.key === "ArrowDown"){
-        newTop = newTop +10
+    } else if (e.key === "ArrowDown") {
+        newTop = newTop + 10
         square.style.top = newTop + "px"
-    } else if (e.key === "ArrowLeft"){
-        newLeft = newLeft -10
+    } else if (e.key === "ArrowLeft") {
+        newLeft = newLeft - 10
         square.style.left = newLeft + "px"
-    } else if (e.key === "ArrowRight"){
-        newLeft = newLeft +10
+    } else if (e.key === "ArrowRight") {
+        newLeft = newLeft + 10
         square.style.left = newLeft + "px"
     }
-
-
 })
-
